1817cd068c57e983e49506b20d45bd5f1f5cdd6c
itachiuchiha-matrix06 <cinephille.45@gmail.com>
2025-10-15 01:18:32 +0530

    upto video number 23 of chai and react learn about ref and forward refrencing

--- 12megaproject/src/components/Header.jsx
+++ 12megaproject/src/components/Header.jsx
@@ -47,6 +47,23 @@ function Header() {
               <Logo width='70-px' />
             </Link>
           </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+              item.active ? (
+                <li key={item.name}>
+                  <button onClick={() => navigate(item.slug)}  className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <Logoutbtn />
+              </li>
+            )}
+          </ul>
+
         </nav>
 
       </Container>
