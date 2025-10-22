acaf022caf5112806944c0e436b9bf7f15a47843
itachiuchiha-matrix06 <cinephille.45@gmail.com>
2025-10-23 01:18:21 +0530

    debug later

--- 12megaproject/src/components/index.js
+++ 12megaproject/src/components/index.js
@@ -3,7 +3,12 @@ import Logoutbtn from "./Logoutbtn"
 import Footer from "./Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-
+import Signup from "./Signup";
+import RTE from "./RTE"
+import Login from "./Login";
+import PostForm from "./PostForm";
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 
 
 export {
@@ -11,5 +16,11 @@ export {
     Footer,
     Container,
     Logo,
-    Logoutbtn
+    Logoutbtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
 }
